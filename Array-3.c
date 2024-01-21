511f8c296afbc89743f6bdba2edc8eccdcbb9bb5
TashrinTanisha <145369121+TashrinTanisha@users.noreply.github.com>
2024-01-21 18:15:38 +0200

    for loop

---
+++ Array-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int number[10] = {2,4,6,8,10,12,14,16,18,20} ;
+
+    int num;
+
+    for(num = 0;num < 10;num++){
+        printf("%d\n",number[num]);
+    }
+
+    return 0;
+}
