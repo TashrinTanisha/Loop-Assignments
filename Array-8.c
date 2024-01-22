74c9f081b3d8a9c319fba362bd8cffe29628b1a3
TashrinTanisha <145369121+TashrinTanisha@users.noreply.github.com>
2024-01-22 13:13:10 +0200

    Array-8 is completed

---
+++ Array-8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int number[10] = {2,4,6,8,10,12,14,16,18,20};
+
+    int num;
+
+    for(num = 0; num < 10; num++)
+    {
+
+        printf("%d\n",number[num]);
+
+    }
+
+    return 0;
+}
